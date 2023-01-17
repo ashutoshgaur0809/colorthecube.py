44e0ae1f6a24039d6504b5f5a710d47bde97a19c
Ashutosh Gaur <121347667+ashutoshgaur0809@users.noreply.github.com>
2023-01-17 14:16:51 +0530

    Add files via upload

---
+++ Color_the_cube.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "1ef6c177",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Enter no test cases= 1\n",
+      "enter no of cubes = 2\n",
+      "there ccolor cost = \n",
+      "1\n",
+      "1\n",
+      "1\n",
+      "1\n",
+      "2\n",
+      "4\n",
+      "Cost= 10.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "T = int(input(\"Enter no test cases= \"))\n",
+    "for i in range(T):\n",
+    "    N = int(input(\"enter no of cubes = \"))\n",
+    "    print(\"there ccolor cost = \")\n",
+    "    x1 = int(input(\"\"))\n",
+    "    x2 = int(input(\"\"))\n",
+    "    x3 = int(input(\"\"))\n",
+    "    x4 = int(input(\"\"))\n",
+    "    x5 = int(input(\"\"))\n",
+    "    x6 = int(input(\"\"))\n",
+    "    cost = (x1+x2+x3+x4+x5+x6)*(N*0.5)\n",
+    "    print(\"Cost=\",cost)         \n",
+    "             \n",
+    "             \n",
+    "             "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "cfd35f17",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
